7048244eab49afb64392e827749e7fbf50a44e37
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-05-30 14:10:34 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Prestamo.BLL;
+using Prestamo.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prestamo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private rCliente cliente = new rCliente();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = cliente;
+        }
+
+        private void Limpiar()
+        {
+            this.cliente = new rCliente();
+            this.DataContext = cliente;
+        }
+
+        private bool Validacion()
+        {
+            bool valido = true;
+
+            if (Prestamoid.Text.Length == 0 || PersonalIdTextBox.Text.Length == 0 ||
+                ConceptoTextBox.Text.Length == 0 || MontoTextBox.Text.Length == 0 || BalanceTextBox.Text.Length == 0 || fechaNDatePicker.Text.Length == 0)
+            {
+                valido = false;
+                MessageBox.Show("Debe llenar todos los campos.", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else if (PersonalIdTextBox.Text.Length == 0 || PersonalIdTextBox.Text.Length < 2)
+            {
+                valido = false;
+                MessageBox.Show("Ingrese un Nombre valido.", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return valido;
+        }
+
+        private void buscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var cliente = ClienteBLL.Buscar(int.Parse(Prestamoid.Text));
+
+            if (cliente != null)
+            {
+                this.cliente = cliente;
+            }
+            else
+            {
+                this.cliente = new rCliente();
+            }
+
+            this.DataContext = this.cliente;
+
+        }
+
+        private void guardarbotton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!Validacion())
+            {
+                return;
+            }
+
+            var key = ClienteBLL.Guardar(cliente);
+
+            if (key)
+            {
+                Limpiar();
+                MessageBox.Show("Se Guardo con Exitosamente ", "Excelente!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("ERROR.", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void editarbotton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void eliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClienteBLL.Eliminar(int.Parse(Prestamoid.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Se a Eliminado." + Prestamoid , "Exitosamente!",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("No se pudo eliminar el registro" + Prestamoid, "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+    }
+}
---
+++ Migrations/20200530092939_Migracion_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Prestamo.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "rCliente",
+                columns: table => new
+                {
+                    Prestamoid = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(nullable: false),
+                    Personaid = table.Column<int>(nullable: false),
+                    Concepto = table.Column<int>(nullable: false),
+                    Monto = table.Column<double>(nullable: false),
+                    Balance = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_rCliente", x => x.Prestamoid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "rCliente");
+        }
+    }
+}
