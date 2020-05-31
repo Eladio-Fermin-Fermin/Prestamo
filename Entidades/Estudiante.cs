52aeaee98f660d3586f9562d4416a2a0291ec2bb
ULYSSESS <ULYSSESS@DESKTOP-MK9933E>
2020-05-31 15:51:00 -0400

    Se creo la Entidad Estudiante

---
+++ Entidades/Estudiante.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Proyecto2.Entidades
+{
+    public class Estudiantes
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Telefono { get; set; }
+        public int Cedula { get; set; }
+        public string Direccion { get; set; }
+        public DateTime FechaNacimiento { get; set; } = DateTime.Now;
+
+        /*[ForeignKey("Id")]
+        public virtual Estudiantes Id { get; set; }*/
+    }
+}
